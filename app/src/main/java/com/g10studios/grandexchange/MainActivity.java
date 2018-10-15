deb2fc78cb2ce7e24cc6c0fbaa4e3b176f621669
Andrew Gieraltowski <37463248+andygten@users.noreply.github.com>
2018-10-14 22:04:24 -0400

    Initial commit

---
+++ app/src/main/java/com/g10studios/grandexchange/MainActivity.java
@@ -0,0 +1,13 @@
+package com.g10studios.grandexchange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
